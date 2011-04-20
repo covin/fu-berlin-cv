bacce6075ea6d22b4c67681032c268a066486ada
stdusr <stdusr@box.(none)>
2011-04-20 18:16:58 +0200

    First revision of u01 task 2

---
+++ lib/rgb2yuv.m
@@ -0,0 +1,23 @@
+function I_yuv = rgb2yuv(I_rgb)
+  red = 1;
+  green = 2;
+  blue = 3;
+  y = 1;
+  u = 2;
+  v = 3;
+
+  [rows, cols, c] = size(I_rgb);
+  if c != 3
+    printf('bad parameter for rgb2yuv: Did you pass a RGB image?\n');
+    return;
+  end
+
+  I_yuv = I_rgb;
+  for i = 1:rows
+    for j = 1:cols
+      I_yuv(i,j,y) = 0.229 * I_rgb(i,j,red) + 0.587 * I_rgb(i,j,green) + 0,144 * I_rgb(i,j,blue);
+      I_yuv(i,j,u) = (I_rgb(i,j,blue) - I_yuv(i,j,y)) * 0.493;
+      I_yuv(i,j,v) = (I_rgb(i,j,red) - I_yuv(i,j,y)) * 0.877;
+    end
+  end
+end
---
+++ u01/u01t2.m
@@ -0,0 +1,8 @@
+# octave script to apply rgb to yuv conversion to an image
+
+printf('-- u01/task2 - RGB to YUV conversion and frequency of colors in UV space\n\n');
+
+# load image into matrix
+I_in = loadimageprompt();
+
+I_out = rgb2yuv(I_in);
