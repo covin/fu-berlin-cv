6e5a47088466122037ab1d2147c3146b23e9a271
covin <covin@gmx.net>
2011-05-15 15:55:42 +0200

    [u04.surf] hc done, paper done, SURF-part left

--- lib/detectCorners.m
+++ lib/detectCorners.m
@@ -1,39 +1,47 @@
-# I - the intensity image
-# N - neighborhood size, has to be odd
+% I - the intensity image
+% N - neighborhood size, has to be odd
+% 
+% returns a matrix of image dimension classifying each pixel M(y,x) as
+% 0 - nothing
+% 1 - edge
+% 2 - corner
 function M = detectCorners(I, N)
-
-[r c] = size(I);
-W = gaussmatrix(N,N/2);
-N2 = (N-1)/2;
-[Dr Dc] = gradient(I);
-M = zeros(r,c);
-
-function ret = bigLambda(l)
-    ret = l > 10;
+    if mod(N,2) == 0
+        printf('unsupported neighborhood size in corner detector');
+        return;
+    endif
+    
+    [r c] = size(I);
+    W = gaussmatrix(N,N/2);
+    N2 = (N-1)/2;
+    [Dr Dc] = gradient(I);
+    M = zeros(r,c);
+    
+    for i=N2+1:r-N2
+      for j=N2+1:c-N2
+        % create windows around current pixel (j,i)
+        Ix = Dr((i-N2):(i+N2),(j-N2):(j+N2));
+        Iy = Dc((i-N2):(i+N2),(j-N2):(j+N2));
+    
+        % see http://www.cse.yorku.ca/~kosta/CompVis_Notes/harris_detector.pdf
+        % CAREFUL: this is not be the same corner detector as in the SURF-paper,
+        % this is from the Harris-Paper
+        S1 = sum(sum(Ix.^2 .* W));
+        S2 = S3 = sum(sum(Ix.*Iy.*W)); 
+        S4 = sum(sum(Iy.^2 .* W));
+        C = [S1, S2 ; S3, S4];
+        % is there any scaling possibility for better 'big lambda' decision?
+        l = eig(C);
+        % search for big eigenvalues, one big -> edge, two big -> corner 
+        if bigLambda(l(1)) && bigLambda(l(2))
+            M(i,j) = 2;
+        elseif bigLambda(l(1)) || bigLambda(l(2))
+            M(i,j) = 1;
+        endif;
+      endfor
+    endfor
 end
 
-for i=N2+1:r-N2
-  for j=N2+1:c-N2
-    %TODO: think of a way to have two equally sized matrices, or is this okay?
-    % by this imean
-    Ix = Dr((i-N2):(i+N2),(j-N2):(j+N2));
-    Iy = Dc((i-N2):(i+N2),(j-N2):(j+N2));
-
-    S1 = sum(sum(Ix.^2 .* W));
-    S2 = S3 = sum(sum(Ix.*Iy.*W)); 
-    S4 = sum(sum(Iy.^2 .* W));
-    l = eig([S1, S2 ; S3, S4]);
-
-    if bigLambda(l(1)) && bigLambda(l(2))
-        M(i,j) = 2;
-    elseif bigLambda(l(1)) || bigLambda(l(2))
-        M(i,j) = 1;
-    endif;
-  endfor
-endfor
-%TODO: search for big eigenvalues, one big -> edge, two big -> corner 
-% see http://www.cse.yorku.ca/~kosta/CompVis_Notes/harris_detector.pdf
-% CAREFUL: this is not be the same corner detector as in the SURF-paper,
-% this is from the Harris-Paper, or see
-% http://en.wikipedia.org/wiki/Corner_detection#The_Harris_.26_Stephens_.2F_Plessey_.2F_Shi-Tomasi_corner_detection_algorithm
+function ret = bigLambda(l)
+    ret = l > 25;
 end
--- u04.surf/u04t08.m
+++ u04.surf/u04t08.m
@@ -8,7 +8,7 @@ if (size(size(I))(2)>2)
   printf("Need grayscale image!")
 endif
 
-C = detectCorners(I,9);
+C = detectCorners(I,5);
 
 # Visualisation: use HSV space
 Out = zeros(rows, cols, 3);
@@ -19,4 +19,4 @@ Out(:,:,3) = I(:,:) ./ 255;
 Out = hsv2rgb(Out);
 
 imshow(Out);
-
+imwrite(Out, 'u04.surf/u04t08.png');
