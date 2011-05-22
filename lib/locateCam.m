2d4921ea490bc7f96d3024e661ba8a07f6fc6514
covin <covin@gmx.net>
2011-05-22 21:56:38 +0200

    [u05] completed (?)

--- lib/locateCam.m
+++ lib/locateCam.m
@@ -15,15 +15,15 @@ function [R t] = locateCam(I, W)
      F = [F; W(p,x) W(p,y) 1      0      0 0 -I(p,x)*W(p,x) -I(p,x)*W(p,y) -I(p,x)];
      F = [F;      0      0 0 W(p,x) W(p,y) 1 -I(p,y)*W(p,x) -I(p,y)*W(p,y) -I(p,y)];
   end
-  h = F(:,1:8)^-1 * F(:,9);
-  h1 = [h(1), h(4), h(7)];
-  h2 = [h(2), h(5), h(8)];
-  h3 = [h(3), h(6),    1];
-  H = [h1' h2' h3'] /norm(h1);
+  h = inv(F(:,1:8))*-F(:,9);
+  H = [h(1) h(2) h(3)
+       h(4) h(5) h(6)
+       h(7) h(8)   1];
+  H /= norm(H(:,1));
   r1 = H(:,1);
   r2 = H(:,2);
   r3 = cross(r1, r2);
-  R = [r1 r2 r3];
-  t = -R^-1*H(:,3);
+  R  = [r1 r2 r3];
+  t  = -R^-1*H(:,3);
 end
 
--- u05.calibration/u05.m
+++ u05.calibration/u05.m
@@ -2,15 +2,15 @@
 % P5100207s: P1(400|160), P2(463|343), P3(166|391), P4(118|190)
 Pic1 = [400, 160; 463, 343; 166, 391; 118, 190];
 % P5100208s: P1(427|143), P2(567|300), P3(185|376), P4( 88|222)
-Pic2 = [427, 143; 567, 300; 185, 376;  88, 222];
+Pic2 = [427, 173; 567, 300; 185, 376;  88, 222];
 % P5100209s: P1(426|166), P2(589|269), P3(177|344), P4( 65|215)
 Pic3 = [426, 166; 589, 269; 177, 344;  65, 215];
 % P5100210s: P1(435|166), P2(611|231), P3(185|279), P4( 63|196)
 Pic4 = [435, 166; 611, 231; 185, 279;  69, 196];
-World = [ 10, 10
-         220, 10
-         220,317
-         317, 10];
+World = [ 10,  10
+          10, 220
+         317, 220
+         317,  10];
 % sensor dimension of [17.3mm x 13.0mm]
 dim = [640, 480];
 sen = [17.3, 13];
@@ -19,11 +19,26 @@ f   = 38;
 function S = pixel2sensor(P, Dim, Sensor)
     S = (P / diag(Dim)-0.5) * diag(Sensor);
 end
-
+format = 'Cam position in pic %d: x=%.2f cm,y=%.2f cm, h=%.2f cm\n'; 
 S = pixel2sensor(Pic1, dim, sen)./f;
 [R t] = locateCam(S,World);
-t
+printf( format, 1, t(1)/10, t(2)/10, t(3)/10);
+O = (R*[0 0 1]')'
+
 
 S = pixel2sensor(Pic2, dim, sen)./f;
 [R t] = locateCam(S,World);
-t
+printf(format,  2, t(1)/10, t(2)/10, t(3)/10);
+O = (R*[0 0 1]')'
+
+S = pixel2sensor(Pic3, dim, sen)./f;
+[R t] = locateCam(S,World);
+printf(format,  2, t(1)/10, t(2)/10, t(3)/10);
+O = (R*[0 0 1]')'
+
+S = pixel2sensor(Pic4, dim, sen)./f;
+[R t] = locateCam(S,World);
+printf(format,  2, t(1)/10, t(2)/10, t(3)/10);
+O = (R*[0 0 1]')'
+
+
