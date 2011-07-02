c81fd791b4f42eb494cb5d2d01e81cf287af9634
stdusr <freak-mbs@web.de>
2011-07-02 13:57:58 +0200

    deskew

---
+++ project/src/main/java/de/fumanoids/features/Field.java
@@ -0,0 +1,18 @@
+package de.fumanoids.features;
+
+public class Field {
+	
+	private int length = 600;
+	private int width = 400;
+	private int line_width = 5;
+	private int goal_width = 150;
+	private int goalpost_width = 10;
+	private int center_circle_diam = 120;
+	private int goal_area_length = 60;
+	private int goal_area_width = 300;
+	private int goal_center = 190; 
+	
+	public Field() {
+		
+	}
+}
